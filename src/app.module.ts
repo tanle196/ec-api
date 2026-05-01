36740e04d5c1bf9922efb60ecdc3a48d38c6b0f1
Tan - Personal <tan-personal@Mac-mini-cua-Tan.local>
2026-05-01 15:53:45 +0700

    Remove nouse code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
 import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { AppConfigModule } from './config/config.module';
@@ -20,7 +19,6 @@ import { PermissionsModule } from './permissions/permissions.module';
     RolesModule,
     PermissionsModule,
   ],
-  controllers: [AppController],
   providers: [
     AppService,
     {
